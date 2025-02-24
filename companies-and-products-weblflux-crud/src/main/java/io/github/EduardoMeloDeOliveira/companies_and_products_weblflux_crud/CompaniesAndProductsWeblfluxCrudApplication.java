2c463cb0f4d6ab33682b36fadb3ccc5e1ce9893a
EduardoMeloDeOliveira <eduardo.moliveira@sptech.school>
2025-02-24 10:28:05 -0300

    checkpoint

---
+++ companies-and-products-weblflux-crud/src/main/java/io/github/EduardoMeloDeOliveira/companies_and_products_weblflux_crud/CompaniesAndProductsWeblfluxCrudApplication.java
@@ -0,0 +1,13 @@
+package io.github.EduardoMeloDeOliveira.companies_and_products_weblflux_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompaniesAndProductsWeblfluxCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompaniesAndProductsWeblfluxCrudApplication.class, args);
+	}
+
+}
---
+++ companies-and-products-weblflux-crud/src/test/java/io/github/EduardoMeloDeOliveira/companies_and_products_weblflux_crud/CompaniesAndProductsWeblfluxCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.EduardoMeloDeOliveira.companies_and_products_weblflux_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompaniesAndProductsWeblfluxCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
